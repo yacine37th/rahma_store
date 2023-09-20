73d763147961cd759b7321cde46bf801b0ed54b7
yacine37th <y.titouah@esi-sba.dz>
2023-09-20 14:53:07 +0100

    first commit

---
+++ my-project/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+  <div className='flex p-0 m-0 bg-red-600 top-0'>
+  <p className='flex p-0 m-0 bg-blue-600'>dededeede</p>
+  </div>
+    </>
+  )
+}
+
+export default App
